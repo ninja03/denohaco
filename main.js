b4d28ca687fc0b7d8bece15a22bc575c9d00f12f
ninja03 <69106571+ninja03@users.noreply.github.com>
2021-08-31 22:34:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,42 +3,57 @@ import { connect } from "https://deno.land/x/redis/mod.ts"
 import { Client } from "https://deno.land/x/mysql/mod.ts"
 
 class MyServer extends Server {
+
   async api(path, req) {
-    // Redis
-    this.redis = await connect({
+    switch (path) {
+      case "/api/rcount": {
+        const redis = await this.getRedis()
+        return { count: await this.getRedisCount(redis) }
+      }
+      case "/api/rup": {
+        const redis = await this.getRedis()
+        await redis.set("count", (await this.getRedisCount(redis)) + 1)
+        return {}
+      }
+      case "/api/mcount": {
+        const mysql = await this.getMySQL()
+        return { count: await this.getMySQLCount(mysql) }
+      }
+      case "/api/mup": {
+        const mysql = await this.getMySQL()
+        await mysql.execute(`insert into click() values()`);
+        return {}
+      }
+    }
+  }
+
+  async getRedis() {
+    return await connect({
       hostname: Deno.env.get("REDIS_HOST") ?? "127.0.0.1",
       port: 6379,
-      password: Deno.env.get("REDIS_PASS") ?? undefined,
+      password: Deno.env.get("REDIS_PASS")
     })
-    let rcount = await this.redis.get("count")
-    if (rcount != undefined) {
-      rcount = parseInt(rcount)
-    } else {
-      rcount = 0
-    }
+  }
 
-    // MySQL
-    const client = await new Client().connect({
+  async getRedisCount(redis) {
+    const rcount = await redis.get("count")
+    if (rcount == undefined) {
+      return 0
+    }
+    return Number(rcount)
+  }
+
+  async getMySQL() {
+    return await new Client().connect({
       hostname: Deno.env.get("MYSQL_HOST") ?? "127.0.0.1",
       username: "root",
       db: "counter",
       password: Deno.env.get("MYSQL_PASS") ?? "pass",
     })
+  }
 
-    const mcount = (await client.query("select count(*) as c from click"))[0].c
-
-    switch (path) {
-      case "/api/rcount":
-        return { count: rcount }
-      case "/api/rup":
-        await this.redis.set("count", rcount + 1)
-        return {}
-      case "/api/mcount":
-        return { count: mcount }
-      case "/api/mup":
-        await client.execute(`insert into click() values()`);
-        return {}
-    }
+  async getMySQLCount(client) {
+    return (await client.query("select count(*) as c from click"))[0].c
   }
 }
 
